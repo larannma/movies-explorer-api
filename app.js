467a2971b6740f21dd44b6e1497f4914c9af53e1
Anna Larionova <larionova.a2000@gmail.com>
2023-11-08 09:34:30 +0300

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
 const express = require('express');
-// Слушаем 3000 порт
-const { PORT } = process.env;
+const mongoose = require('mongoose');
 
-const app = express();
-
-app.listen(PORT, () => {
-    // Если всё работает, консоль покажет, какой порт приложение слушает
-    console.log(`App listening on port ${PORT}`)
-})
